c0e0ff91ec4ed049a0b65cffc40a4f362bb983f1
Fadyy22 <fadyalaa441@gmail.com>
2024-01-02 22:54:43 +0200

    then catch finally

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,16 @@
+export default function handleResponseFromAPI(promise) {
+  promise
+    .then(() => {
+      return {
+        status: 200,
+        body: 'success',
+      };
+    })
+    .catch(() => {
+      console.log('Got a response from the API');
+      return new Error();
+    })
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
+} 
