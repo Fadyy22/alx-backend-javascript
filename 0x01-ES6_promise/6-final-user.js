1fe2399e053b9e62ec4eb4dd9802610301197280
Fadyy22 <fadyalaa441@gmail.com>
2024-01-04 00:23:54 +0200

    all settled

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,10 +4,10 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise
     .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then(values => {
-      return values.map(req => ({
+    .then((values) => {
+      values.map((req) => ({
         status: req.status,
         value: req.status === 'fulfilled' ? req.value : req.reason,
-      }))
-    })
+      }));
+    });
 }
