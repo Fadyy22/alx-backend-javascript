1ef649f53fabfa76a15d3a429f05e5f65e2fba22
Fadyy22 <fadyalaa441@gmail.com>
2024-01-04 01:00:54 +0200

    async await

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,11 @@
+import { uploadPhoto, createUser } from "./utils";
+
+export default async function asyncUploadUser() {
+  const uploadResult = await uploadPhoto();
+  const createResult = await createUser();
+
+  return {
+    photo: uploadResult,
+    user: createResult,
+  };
+}
