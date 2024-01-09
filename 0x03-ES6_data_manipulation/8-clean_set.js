b0bfd870395ab664461bebda3229df848c10b5fe
Fadyy22 <fadyalaa441@gmail.com>
2024-01-09 15:29:46 +0200

    return string of all set values that starts with specific string

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,6 @@
 export default function cleanSet(set, startString) {
   const newString = [];
-  if (!(set instanceof Set)) {
+  if (typeof startString !== 'string') {
     return '';
   }
   set.forEach((string) => {
