0517f069e86edcf538b0fdc9aa7c93f92ec66f15
Fadyy22 <fadyalaa441@gmail.com>
2024-01-10 20:24:53 +0200

    add check if employee is director and execute work function

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -7,7 +7,7 @@ interface DirectorInterface {
 interface TeacherInterface {
   workFromHome(): string;
   getCoffeeBreak(): string;
-  workDirectorTasks(): string;
+  workTeacherTasks(): string;
 }
 
 class Director implements DirectorInterface {
@@ -33,7 +33,7 @@ class Teacher implements TeacherInterface {
     return "Cannot have a break";
   }
 
-  workDirectorTasks(): string {
+  workTeacherTasks(): string {
     return "Getting to work";
   }
 }
@@ -43,4 +43,15 @@ function createEmployee(salary: number | string): Director | Teacher {
     return new Teacher();
   }
   return new Director();
+}
+
+function isDirector(employee: Director | Teacher): boolean {
+  return employee instanceof Director;
+}
+
+function executeWork(employee: Director | Teacher) {
+  if (isDirector(employee)) {
+    return (employee as Director).workDirectorTasks;
+  }
+  return (employee as Teacher).workTeacherTasks();
 }
