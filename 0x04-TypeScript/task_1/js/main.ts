181dc4c758dfd534d0d0c8264b4c01baa032621e
Fadyy22 <fadyalaa441@gmail.com>
2024-01-10 19:04:21 +0200

    add class student and interface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -18,3 +18,26 @@ interface printTeacherFunction {
 function printTeacher(firstName: string, lastName: string): string {
   return `${firstName[0]}. ${lastName}`;
 }
+
+interface IStudent {
+  workOnHome(): string;
+  displayName(): string;
+}
+
+class StudentClass implements IStudent {
+  private firstName: string;
+  private lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHome() {
+    return "Currently working";
+  }
+
+  displayName() {
+    return this.firstName;
+  }
+}
