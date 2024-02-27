103cae73aca217bee55ddf30bf8b2204f85e8316
Fadyy22 <fadyalaa441@gmail.com>
2024-02-28 01:04:59 +0200

    read csv file async way

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -5,22 +5,24 @@ const countStudents = (path) => new Promise((resolve, reject) => {
     if (err) {
       reject(new Error('Cannot load the database'));
     }
-    const db = data.split('\n').map((row) => row.split(','));
-    const students = db.slice(1, -1);
-    const fields = {};
-    for (const student of students) {
-      if (fields[student[3]]) {
-        fields[student[3]].push(student[0]);
-      } else {
-        fields[student[3]] = [student[0]];
+    if (data) {
+      const db = data.split('\n').map((row) => row.split(','));
+      const students = db.slice(1, -1);
+      const fields = {};
+      for (const student of students) {
+        if (fields[student[3]]) {
+          fields[student[3]].push(student[0]);
+        } else {
+          fields[student[3]] = [student[0]];
+        }
       }
-    }
 
-    console.log(`Number of students: ${students.length}`);
-    for (const [key, value] of Object.entries(fields)) {
-      console.log(`Number of students in ${key}: ${value.length}. List: ${value.join(', ')}`);
+      console.log(`Number of students: ${students.length}`);
+      for (const [key, value] of Object.entries(fields)) {
+        console.log(`Number of students in ${key}: ${value.length}. List: ${value.join(', ')}`);
+      }
+      resolve(true);
     }
-    resolve(true);
   });
 });
 
