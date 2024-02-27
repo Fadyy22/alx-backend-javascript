a7cbbdf8ac879b586860862e6c993fae132c3792
Fadyy22 <fadyalaa441@gmail.com>
2024-02-28 01:03:16 +0200

    read csv file async way

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+
+const countStudents = (path) => new Promise((resolve, reject) => {
+  fs.readFile(path, 'utf-8', (err, data) => {
+    if (err) {
+      reject(new Error('Cannot load the database'));
+    }
+    const db = data.split('\n').map((row) => row.split(','));
+    const students = db.slice(1, -1);
+    const fields = {};
+    for (const student of students) {
+      if (fields[student[3]]) {
+        fields[student[3]].push(student[0]);
+      } else {
+        fields[student[3]] = [student[0]];
+      }
+    }
+
+    console.log(`Number of students: ${students.length}`);
+    for (const [key, value] of Object.entries(fields)) {
+      console.log(`Number of students in ${key}: ${value.length}. List: ${value.join(', ')}`);
+    }
+    resolve(true);
+  });
+});
+
+module.exports = countStudents;
