efa3ab23c473c3812d179c68dfdf15a65015c316
Fadyy22 <fadyalaa441@gmail.com>
2024-03-01 01:01:21 +0200

    add tests with stubs

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,18 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+const sendPaymentRequestToApi = require('./3-payment');
+const Utils = require('./utils');
+
+describe('sendPaymentRequestToApi', function () {
+  it('sendPaymentRequestToApi uses calculateNumber from Utils and returns 10 and logs', function () {
+    const consoleSpy = sinon.spy(console);
+    const calculateNumberStub = sinon.stub(Utils, 'calculateNumber');
+
+    calculateNumberStub.returns(10);
+    sendPaymentRequestToApi(100, 20);
+    expect(calculateNumberStub.calledWith('SUM', 100, 20)).to.be.true;
+    expect(consoleSpy.log.calledWith('The total is: 10')).to.be.true;
+    calculateNumberStub.restore();
+    consoleSpy.log.restore();
+  });
+});
