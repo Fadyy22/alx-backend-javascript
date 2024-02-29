70d5cf388441266053b3e2cab5fe469decaa5082
Fadyy22 <fadyalaa441@gmail.com>
2024-03-01 00:26:42 +0200

    add tests with sinon

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,15 @@
+const { expect } = require('chai');
+const { spy } = require('sinon');
+const sendPaymentRequestToApi = require('./3-payment');
+const Utils = require('./utils');
+
+describe('sendPaymentRequestToApi', function () {
+  it('sendPaymentRequestToApi uses calculateNumber from Utils', function () {
+    const utilsSpy = spy(Utils);
+
+    sendPaymentRequestToApi(100, 20);
+    expect(utilsSpy.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
+    expect(utilsSpy.calculateNumber.calledOnce).true;
+    utilsSpy.calculateNumber.restore();
+  });
+})
